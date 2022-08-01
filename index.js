7b5e5ff607e3644e0a8c5f7209c9e1d853bca156
boycany <yurayurateiko@gmail.com>
2022-08-01 15:42:58 +0800

    route setting over

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv")
+dotenv.config()
+
+const authRoute = require("./routes").auth
+
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongodb.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use("/api/user", authRoute)
+
+
+app.listen(process.env.SERVER_PORT || 8000, () => {
+  console.log(`Server is running on ${process.env.SERVER_PORT || 8000}.`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const router = require("express").Router()
+
+router.use((req, res, next)=>{
+    console.log("A request is coming in to auth.js");
+    next()
+})
+
+/*   url: localhost/api/user/test/API   */
+router.get("/testAPI", (req, res)=>{
+    const msgObj = {
+        message: "Test API is working."
+    }
+    return res.json(msgObj)
+})
+
+module.exports = router
